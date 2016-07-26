47f2dd6d5b92b858a4684ba09c3914af5e6b1ccb
jx <237603242@qq.com>
2016-07-26 16:53:11 +0800

    commit first

---
+++ GitHubHelloWorld/GitHubHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubHelloWorld
+//
+//  Created by jx on 16/7/26.
+//  Copyright © 2016年 jx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubHelloWorld/GitHubHelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubHelloWorld
+//
+//  Created by jx on 16/7/26.
+//  Copyright © 2016年 jx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
